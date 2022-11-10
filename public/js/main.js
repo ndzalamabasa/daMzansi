baf7e913e42e6290af49d7d3b5847c07f30040a8
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-10 17:43:48 +0200

    added more classes for styling the modal

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector(".modal").classList.add("flex","flex-col","items-center","justify-center","text-center","bg-sky-600","w-11/12","max-w-xl","m-auto")})();
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+const modal = document.querySelector('.modal');
+
+modal.classList.add('flex', 'flex-col', 'items-center', 'justify-center', 'text-center', 'bg-sky-600', 'w-11/12', "max-w-xl", "m-auto");

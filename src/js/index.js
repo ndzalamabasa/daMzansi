a06caf79606f29952ebe2c4dd705d286d4e13be0
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-12 13:28:02 +0200

    added array of instructions

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
 const modal = document.querySelector('.modal');
+const infoContainer = document.getElementById('info');
+const gameInfo = [
+  'A board game that teaches you about South Africa.',
+  'The game takes the form of snakes and ladders, where you click a button to get the number of moves you can take (generated randomly between 1 and 6).',
+];
 
+infoContainer.innerHTML = gameInfo[0];
 modal.classList.add(
   'flex',
   'flex-col',

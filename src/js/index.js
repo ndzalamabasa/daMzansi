eeca990d35e9d8bb9f8f7f79a9f6a01c1f17213d
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-13 15:59:06 +0200

    added functionality to select an avatar

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
+const avatars = document.querySelectorAll('.avatar');
+const avatarPath = './assets/images/avatars/';
 
+avatars.forEach((avatar) => {
+  avatar.addEventListener('click', (e) => {
+    const target = e.target;
+    const src = target.getAttribute('src');
+    const lastSlash = src.lastIndexOf('/');
+    const lastPeriod = src.lastIndexOf('.');
+    const avatarName = src.slice(lastSlash + 1, lastPeriod);
+    const avatarExt = src.slice(lastPeriod + 1);
+    console.log(src);
+    console.log(!src.includes('selected'));
+
+    if (!src.includes('selected')) {
+      const avatarSrc = `${avatarPath}${avatarName}-selected.${avatarExt}`;
+      target.setAttribute('src', avatarSrc);
+      console.log(avatarSrc);
+    }
+  });
+});

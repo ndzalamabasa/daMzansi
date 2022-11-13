007d2b4ee27ba850b6a0083d5cd7256accee8fb9
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-13 13:24:30 +0200

    commented out unused code currently

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
 const modal = document.querySelector('.modal');
 const infoContainer = document.getElementById('info');
+// const next = document.getElementById('next');
 const gameInfo = [
-  'A board game that teaches you about South Africa.',
+  'Da Mzansi is a board game that teaches you about South Africa.',
   'The game takes the form of snakes and ladders, where you click a button to get the number of moves you can take (generated randomly between 1 and 6).',
 ];
 
-infoContainer.innerHTML = gameInfo[0];
+// gameInfoCopy = gameInfo.slice();
+
+// infoContainer.innerHTML = gameInfo.shift();
+// next.addEventListener('click', () => {
+//   if (gameInfo.length > 0) {
+//     infoContainer.innerHTML = gameInfo.shift();
+//   } else {
+//     next.style.display = 'none';
+//   }
+// });
+
 modal.classList.add(
   'flex',
   'flex-col',

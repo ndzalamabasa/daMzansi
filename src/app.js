da1c65e9d4b2bd816215ae7ccd51b7d4b05cfe79
root <root@localhost.localdomain>
2022-11-14 07:57:17 +0200

    database

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-import express from 'express';
+import express from 'express'
+import mongoose from 'mongoose'
 
-const PORT = 3000;
+const PORT = 3000
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  
-});
+app.get('/', (req, res) => {})
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
-
+  console.log(`Server running on port ${PORT}`)
+})

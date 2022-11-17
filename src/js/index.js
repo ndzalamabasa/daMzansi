348b68a36d28a45c0c8fb379d86c5d4e6c6be328
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-17 16:50:57 +0200

    added play initiator

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,39 @@
-import { UserProfile } from './userAvatar.js'
+import { UserProfile } from './userAvatar.js';
 
-const avatars = document.querySelectorAll('.avatar')
-const avatarPath = './assets/images/avatars/'
-let user
-
+const avatars = document.querySelectorAll('.avatar');
+const avatarPath = './assets/images/avatars/';
+let user;
+console.log('hello');
 avatars.forEach((avatar) => {
   avatar.addEventListener('click', (e) => {
-    const target = e.target
-    const src = target.getAttribute('src')
-    const lastSlash = src.lastIndexOf('/')
-    const lastPeriod = src.lastIndexOf('.')
-    let avatarName = src.slice(lastSlash + 1, lastPeriod)
-    const avatarExt = src.slice(lastPeriod + 1)
+    const target = e.target;
+    const src = target.getAttribute('src');
+    const lastSlash = src.lastIndexOf('/');
+    const lastPeriod = src.lastIndexOf('.');
+    let avatarName = src.slice(lastSlash + 1, lastPeriod);
+    const avatarExt = src.slice(lastPeriod + 1);
 
     if (!src.includes('selected')) {
-      const selectedAvatar = `${avatarPath}${avatarName}-selected.${avatarExt}`
-      target.setAttribute('src', selectedAvatar)
+      const selectedAvatar = `${avatarPath}${avatarName}-selected.${avatarExt}`;
+      target.setAttribute('src', selectedAvatar);
       user = new UserProfile(
         avatarName,
-        `${avatarPath}${avatarName}.${avatarExt}`,
-      )
+        `${avatarPath}${avatarName}.${avatarExt}`
+      );
     } else {
-      avatarName = avatarName.replace('-selected', '')
-      const defaultAvatar = `${avatarPath}${avatarName}.${avatarExt}`
-      target.setAttribute('src', defaultAvatar)
+      avatarName = avatarName.replace('-selected', '');
+      const defaultAvatar = `${avatarPath}${avatarName}.${avatarExt}`;
+      target.setAttribute('src', defaultAvatar);
     }
-  })
-})
+  });
+});
+
+const playButton = document.getElementById('playButton');
+
+playButton.addEventListener('click', () => {
+  if (user) {
+    window.location.href = `./game.html?user=${user.userName}&avatar=${user.userAvatar}`;
+  } else {
+    alert('Please select an avatar to continue');
+  }
+});

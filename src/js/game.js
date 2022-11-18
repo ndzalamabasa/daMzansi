e67b62c2a94af3ea34cf45b9866f8ab7c32df881
Tshepo <tshepo@localhost.localdomain>
2022-11-18 11:51:55 +0200

    added stuff

--- src/js/game.js
+++ src/js/game.js
@@ -1,48 +1,60 @@
-const userName = new URLSearchParams(window.location.search).get('user');
-const userAvatar = new URLSearchParams(window.location.search).get('avatar');
-const board = document.getElementById('board');
-const cardsPath = './assets/images/cards/';
-const cardsArray = [];
-
+const userName = new URLSearchParams(window.location.search).get('user')
+const userAvatar = new URLSearchParams(window.location.search).get('avatar')
+const board = document.getElementById('board')
+const cardsPath = './assets/images/cards/'
+const cardsArray = []
+import { UserProfile } from './userAvatar.js'
 // fetch('data.json')
 //   .then((response) => response.json())
 //   .then((data) => {
 //     addQuestionsToBoard(data);
 //   });
-
-const cardsLetters = ['A', 'B', 'C', 'D', 'E'];
+const avatar = document.createElement('img')
+const cardsLetters = ['A', 'B', 'C', 'D', 'E']
 
 function createCards(letter, cardsArray) {
   for (let i = 1; i <= 10; i++) {
-    cardsArray.push(`${cardsPath}${letter}${i}.svg`);
+    cardsArray.push(`${cardsPath}${letter}${i}.svg`)
   }
 }
 
 cardsLetters.forEach((letter) => {
-  createCards(letter, cardsArray);
-});
+  createCards(letter, cardsArray)
+})
 
 function addCardsToBoard(cardsArray) {
-  board.innerHTML = '';
-  let id = 1;
+  board.innerHTML = ''
+  let id = 1
   cardsArray.forEach((card) => {
-    const cardDiv = document.createElement('div');
-    const cardImage = document.createElement('img');
-    const avatar = document.createElement('img');
-    cardDiv.classList.add('card', 'h-28', 'w-28', 'rounded-lg', 'relative');
-    cardDiv.setAttribute('id', id);
-    cardImage.setAttribute('src', card);
-    avatar.setAttribute('src', userAvatar);
-    avatar.classList.add('absolute', 'top-2.5', 'right-2.5', 'h-12', 'w-12');
-    cardDiv.appendChild(cardImage);
-    cardDiv.appendChild(avatar);
-    id++;
+    const cardDiv = document.createElement('div')
+    const cardImage = document.createElement('img')
 
-    board.appendChild(cardDiv);
-  });
+    cardDiv.classList.add('card', 'h-28', 'w-28', 'rounded-lg', 'relative')
+    cardDiv.setAttribute('id', id)
+    cardImage.setAttribute('src', card)
+    avatar.setAttribute('src', userAvatar)
+    // avatar.classList.add('absolute', 'top-2.5', 'right-2.5', 'h-12', 'w-12')
+    cardDiv.appendChild(cardImage)
+    // cardDiv.appendChild(avatar)
+    id++
+
+    board.appendChild(cardDiv)
+  })
 }
 
-addCardsToBoard(cardsArray);
+let currentNumber = 0
+let diceNumber = document.getElementById('diceNumber').innerHTML
+const user = new UserProfile(userName, avatar, 0)
+const moveButton = document.getElementById('moveAround')
+moveButton.onclick = () => {
+  const number = Math.floor(1 + Math.random() * 6)
+  user.position = user.position + number
+  document.getElementById(user.position).appendChild(avatar)
+  avatar.classList.add('absolute', 'top-2.5', 'right-2.5', 'h-12', 'w-12')
+  console.log(user)
+}
+
+addCardsToBoard(cardsArray)
 
 // function addQuestionsToBoard(questions) {
 //   board.innerHTML = '';

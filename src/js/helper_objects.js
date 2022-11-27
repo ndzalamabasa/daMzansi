6e579da4cdbc96609733a7a0fbeb9deb1a9fb4d1
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-27 22:55:14 +0200

    added board element

--- src/js/helper_objects.js
+++ src/js/helper_objects.js
@@ -2,6 +2,7 @@ const domElements = {
   popUpContainer: document.getElementById('pop-up-container'),
   popUp: document.getElementById('pop-up'),
   rollDiceButton: document.getElementById('roll-dice'),
+  board: document.getElementById('board'),
 };
 
 export { domElements };

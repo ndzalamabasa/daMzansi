a103ff0d704b2823917e96b80a5bf77ff9004db9
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-28 12:43:24 +0200

    reconstructed the move button to emulate roll dice button styles

--- src/js/game_controllers.js
+++ src/js/game_controllers.js
@@ -1,24 +1,11 @@
-const moveButton = document.createElement('button');
+import { domElements } from './helper_objects.js';
+const { rollDiceButton } = domElements;
+const moveButton = rollDiceButton.cloneNode(true);
 const cardsPath = './assets/images/cards/';
 const cardsArray = [];
 const cardsLetters = ['A', 'B', 'C', 'D', 'E'];
 const avatar = document.createElement('img');
 
-moveButton.setAttribute('id', 'moveAround');
-moveButton.classList.add(
-  'bg-[url("/assets/images/btn-primary-bg.svg")]',
-  'bg-cover',
-  'rounded-xl',
-  'py-2.5',
-  'px-10',
-  'mt-4',
-  'text-xl',
-  'font-semibold',
-  'text-slate-200',
-  'hover:scale-95',
-  'transition-all',
-  'duration-200'
-);
 moveButton.innerText = 'Move';
 
 cardsLetters.forEach((letter) => {

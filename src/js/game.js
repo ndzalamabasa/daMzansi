62029d228bac67e969601fa3c1293e646a320c39
ndzalamabasa <ndzalamabasa@gmail.com>
2022-11-29 15:17:20 +0200

    removed unused code

--- src/js/game.js
+++ src/js/game.js
@@ -1,39 +1,12 @@
-import { avatarArray } from './avatarArray.js';
-import { avatar, cardsArray, move, user } from './game_controllers.js';
-import {
-  showPopUp,
-  addCardsToBoard,
-  rollDiceResults,
-  rollDice,
-} from './helper_functions.js';
+import { avatar, cardsArray } from './game_controllers.js';
+import { addCardsToBoard, rollDice } from './helper_functions.js';
 import { domElements } from './helper_objects.js';
-const { board, popUp, rollDiceButton } = domElements;
+const { board, rollDiceButton } = domElements;
 let userAvatar = new URLSearchParams(window.location.search).get('avatar');
 avatar.setAttribute('src', userAvatar);
 
-// user.setUserAvatar()
 rollDiceButton.addEventListener('click', rollDice);
 
 addCardsToBoard(cardsArray).forEach((card) => {
-  // <<<<<<< HEAD
   board.appendChild(card);
 });
-// =======
-//   board.appendChild(card)
-// })
-
-// function rollDice() {
-//   console.log(avatar)
-//   avatar.setAttribute('src', user.userAvatar)
-
-//   showPopUp()
-//   const dice = document.createElement('img')
-//   dice.setAttribute('src', './assets/images/dice.svg')
-//   dice.classList.add('h-1/2', 'w-1/2', 'animate-spin')
-//   popUp.appendChild(dice)
-
-//   setTimeout(() => {
-//     rollDiceResults(move)
-//   }, 2000)
-// }
-// >>>>>>> ce89b300f5c18314b5ea2601c1817805ebc7286c
--- src/js/game_controllers.js
+++ src/js/game_controllers.js
@@ -1,74 +1,73 @@
-import { UserProfile } from './userAvatar.js'
-import { getQuestions, hidePopUp } from './helper_functions.js'
-import { addQuestionsToBoard } from './questions.js'
-import { domElements } from './helper_objects.js'
-import { avatarArray } from './avatarArray.js'
+import { UserProfile } from './userAvatar.js';
+import { getQuestions, hidePopUp } from './helper_functions.js';
+import { addQuestionsToBoard } from './questions.js';
+import { domElements } from './helper_objects.js';
+import { avatarArray } from './avatarArray.js';
 
-const userName = new URLSearchParams(window.location.search).get('user')
-const users = new URLSearchParams(window.location.search).get('list') // added
-const { rollDiceButton } = domElements
-const moveButton = rollDiceButton.cloneNode(true)
-const cardsPath = './assets/images/cards/'
+const users = new URLSearchParams(window.location.search).get('list'); // added
+const { rollDiceButton } = domElements;
+const moveButton = rollDiceButton.cloneNode(true);
+const cardsPath = './assets/images/cards/';
 
-const cardsArray = []
-const cardsLetters = ['A', 'B', 'C', 'D', 'E']
+const cardsArray = [];
+const cardsLetters = ['A', 'B', 'C', 'D', 'E'];
 
-let avatar = document.createElement('img')
-avatar.classList.add('absolute', 'top-2.5', 'right-2.5', 'h-12', 'w-12')
-let user
+let avatar = document.createElement('img');
+avatar.classList.add('absolute', 'top-2.5', 'right-2.5', 'h-12', 'w-12');
+let user;
 
-const usersObject = {}
-const avatars = []
+const usersObject = {};
+const avatars = [];
 
 users.split(',').forEach((profile, index) => {
-  const profileNameAndAvatar = profile.split(':')
+  const profileNameAndAvatar = profile.split(':');
   usersObject[profileNameAndAvatar[0]] = new UserProfile(
     profileNameAndAvatar[0],
     profileNameAndAvatar[1],
     0,
     index,
-    avatarArray[index],
-  )
-})
+    avatarArray[index]
+  );
+});
 
-let i = 0
-let userKeys = Object.keys(usersObject)
+let i = 0;
+let userKeys = Object.keys(usersObject);
 function switchTurns() {
-  const userToTurn = usersObject[userKeys[i]]
-  user = userToTurn
-  i++
-  if (i === userKeys.length) i = 0
+  const userToTurn = usersObject[userKeys[i]];
+  user = userToTurn;
+  i++;
+  if (i === userKeys.length) i = 0;
 }
-switchTurns()
+switchTurns();
 
-moveButton.innerText = 'Move'
+moveButton.innerText = 'Move';
 
 cardsLetters.forEach((letter) => {
   for (let i = 1; i <= 10; i++) {
-    cardsArray.push(`${cardsPath}${letter}${i}.svg`)
+    cardsArray.push(`${cardsPath}${letter}${i}.svg`);
   }
-})
+});
 
 function move(numberOfMoves) {
-  hidePopUp()
+  hidePopUp();
 
   if (user.position + numberOfMoves > 50) {
-    let moreThan50 = user.position + numberOfMoves
-    moreThan50 = moreThan50 - 50
-    user.position = user.position - moreThan50
+    let moreThan50 = user.position + numberOfMoves;
+    moreThan50 = moreThan50 - 50;
+    user.position = user.position - moreThan50;
   }
   user.position == 50
     ? alert('You Win')
-    : (user.position = user.position + numberOfMoves)
+    : (user.position = user.position + numberOfMoves);
 
-  user.positionUser(avatars[user.playerTurn])
-  console.log(user)
+  user.positionUser(avatars[user.playerTurn]);
+  console.log(user);
 
   setTimeout(() => {
     getQuestions().then((questions) => {
-      addQuestionsToBoard(questions)
-    })
-  }, 1500)
+      addQuestionsToBoard(questions);
+    });
+  }, 1500);
 }
 
 export {
@@ -80,4 +79,4 @@ export {
   avatars,
   usersObject,
   avatar,
-}
+};
